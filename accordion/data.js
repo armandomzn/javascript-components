3aef2175f549db166b3eaf1c18bf90cc277af865
Armando <armandomm98@hotmail.com>
2023-08-14 23:01:12 -0600

    accordion project modified, html, css and js completed

---
+++ accordion/data.js
@@ -0,0 +1,28 @@
+export const data = [
+  {
+    id: 1,
+    title: "What is JavaScript and what is it used for?",
+    info: "JavaScript is a widely used programming language in web development. It's used to add interactivity, dynamism, and functionality to websites. It allows you to manipulate page content, respond to user events, and make requests to servers.",
+  },
+  {
+    id: 2,
+    title: "What's the difference between null and undefined in JavaScript?",
+    info: "null and undefined are values that indicate the absence of a value. null is a value intentionally assigned by the programmer to indicate that a variable is empty or intentionally devoid of value. undefined is automatically assigned to variables that are declared but not assigned any value.",
+  },
+
+  {
+    id: 3,
+    title: "What's the difference between let, const, and var?",
+    info: "let and const are variable declarations introduced in ES6. let is used to declare variables that can change their value after the initial declaration. const is used to declare variables whose value won't change after the initial declaration. var was the older way of declaring variables and has function-level scope, whereas let and const have block-level scope.",
+  },
+  {
+    id: 4,
+    title: "What is an arrow function in JavaScript?",
+    info: "An arrow function is a more concise way to write functions in JavaScript. They are denoted with () => after the list of arguments. They have a different behavior in relation to the this value and cannot be used as constructors (cannot be called with new).",
+  },
+  {
+    id: 5,
+    title: "What is the Document Object Model (DOM) in JavaScript?",
+    info: "The Document Object Model (DOM) is a tree-like representation of the structure and content of an HTML or XML document. In the context of JavaScript, the DOM allows you to access and manipulate elements on a web page. Interactions with the DOM are what enable JavaScript to dynamically modify the content and style of a web page.",
+  },
+];
--- accordion/index.js
+++ accordion/index.js
@@ -1,8 +1,51 @@
-const questionBtns = document.querySelectorAll(".question-btn");
+import { data } from "./data.js";
 
-questionBtns.forEach((button) => {
-  button.addEventListener("click", () => {
-    const parent = button.parentElement.parentElement;
-    parent.classList.toggle("show-text");
+const questionsContainer = document.querySelector(".questions");
+// we create the questions in the HTML file
+createQuestions(data, questionsContainer);
+const questions = document.querySelectorAll(".question");
+
+function createQuestions(elements, parentDomReference) {
+  // We generate the dynamic questions for the div container
+  const questions = elements.map((question) => {
+    const { title, info } = question;
+    const article = document.createElement("article");
+    article.classList.add("question");
+    article.innerHTML = `
+    <header>
+      <h5>${title}</h5>
+    <!-- toggle button  -->
+    <button class="toggle-btn">
+      <span class="up-button">
+        <i class="fa-solid fa-chevron-up"></i>
+      </span>
+      <span class="down-button">
+        <i class="fa-solid fa-chevron-down"></i>
+      </span>
+    </button>
+    <!-- end toggle button  -->
+  </header>
+  <!-- end header  -->
+  <div class="question-text">
+    <p>
+      ${info}
+    </p>
+  </div>
+    `;
+    return article;
+  });
+  // We add the questions to the div container and replace the elements that we can found in html file
+  parentDomReference.replaceChildren(...questions);
+}
+
+let activeElement = null;
+questions.forEach((question, index) => {
+  const btn = question.querySelector(".toggle-btn");
+  btn.addEventListener("click", (e) => {
+    if (activeElement !== null && index !== activeElement) {
+      questions[activeElement].classList.remove("show-question");
+    }
+    activeElement = index;
+    question.classList.toggle("show-question");
   });
 });
