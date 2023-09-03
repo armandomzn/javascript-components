b114c31772f717fdc6bbbe497e6dc74613482ab9
Armando <armandomm98@hotmail.com>
2023-09-03 16:05:21 -0600

    localstorage added to dark mode project

--- dark_mode/index.js
+++ dark_mode/index.js
@@ -1,3 +1,22 @@
+const lightBtn = document.querySelector(".light");
+const darkBtn = document.querySelector(".dark");
+const getDarkTheme = () => {
+  return localStorage.getItem("darkTheme") || "";
+};
+function setDarkTheme(theme) {
+  localStorage.setItem("darkTheme", theme);
+}
+window.addEventListener("DOMContentLoaded", () => {
+  if (getDarkTheme() === "dark") {
+    document.documentElement.classList.add("dark-mode");
+    lightBtn.classList.remove("hide");
+    darkBtn.classList.add("hide");
+  } else {
+    document.documentElement.classList.remove("dark-mode");
+    lightBtn.classList.add("hide");
+    darkBtn.classList.remove("hide");
+  }
+});
 const btnContainer = document.querySelector(".btn-container");
 btnContainer.addEventListener("click", (e) => {
   console.log();
@@ -9,6 +28,11 @@ btnContainer.addEventListener("click", (e) => {
     }
   });
   document.documentElement.classList.toggle("dark-mode");
+  if (!document.documentElement.classList.contains("dark-mode")) {
+    setDarkTheme("light");
+  } else {
+    setDarkTheme("dark");
+  }
 });
 [...document.querySelectorAll(".date")].forEach((item, index) => {
   const days = 10;
