c6d7c8365a1eb1b70fbb1f615cd4fa83f213ba68
Armando <armandomm98@hotmail.com>
2024-05-18 19:24:30 -0600

    singleCocktailDrink fixed to detect only the button not the entire article in cocktails project

--- cocktails_api/src/setCocktailDrink.js
+++ cocktails_api/src/setCocktailDrink.js
@@ -2,7 +2,7 @@ export const setCocktailDrink = (container) => {
   container.addEventListener("click", (e) => {
     e.preventDefault();
     const id = e.target.parentElement.parentElement.dataset.id;
-    if (!id) {
+    if (!id || !e.target.classList.contains("btn")) {
       return;
     }
     localStorage.setItem("drink", id);
