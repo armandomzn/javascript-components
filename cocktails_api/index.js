e4ac310fc04e14d0de79017a3198bea999de1a9d
Armando <armandomm98@hotmail.com>
2024-11-05 22:18:09 -0600

    Fixing search query in url to deploy some cocktails when page loads.

--- cocktails_api/index.js
+++ cocktails_api/index.js
@@ -1,7 +1,6 @@
 import { showCocktails } from "./src/showCocktails.js";
 import "./src/formCocktails.js";
-
-const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
+const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=b";
 
 window.addEventListener("DOMContentLoaded", () => {
   showCocktails(url);
